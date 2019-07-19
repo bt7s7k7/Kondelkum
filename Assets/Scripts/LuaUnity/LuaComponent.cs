2aa8fa9bbdfe9c29d4e5ec70a8837e8172c16aad
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-07-19 17:00:40 +0200

    Lua integration

---
+++ Assets/Scripts/LuaUnity/LuaComponent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NLua;
+
+namespace LuaUnity {
+	public class LuaComponent : MonoBehaviour {
+
+		[System.Serializable]
+		public struct ImportedObject {
+			public string label;
+			public Object target;
+		}
+
+		public LuaState state;
+		[Multiline]
+		public string code;
+		public ImportedObject[] importedObjects;
+
+		public object FindObject(string name) {
+			foreach (var imported in importedObjects) {
+				if (imported.label == name) {
+					return imported.target;
+				}
+			}
+			return null;
+		}
+
+		[B.MethodButton("Run")]
+		public void Run() {
+			var lua = LuaState.ReadOrGlobal(state);
+			lua["this"] = this;
+			lua.DoString(code);
+			lua["this"] = null;
+		}
+	}
+}
---
+++ Assets/Scripts/LuaUnity/LuaState.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NLua;
+
+namespace LuaUnity {
+
+	public class LuaState : MonoBehaviour {
+		public static Lua global;
+
+		Lua state;
+		public Lua Get() {
+			if (state == null) {
+				state = new Lua();
+				state.LoadCLRPackage();
+				state.DoString("import ('UnityEngine')");
+			}
+			return state;
+		}
+
+
+		public static Lua ReadOrGlobal(LuaState state) {
+			if (state == null) {
+				if (global == null) {
+					global = new Lua();
+					global.LoadCLRPackage();
+					global.DoString("import ('UnityEngine')");
+				}
+				return global;
+			} else {
+				return state.Get();
+			}
+
+		}
+	}
+}
