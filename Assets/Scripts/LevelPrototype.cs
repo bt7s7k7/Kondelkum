b1c8f57d91ca863f2600556550cc0f442b64107f
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-07-19 18:51:28 +0200

    Level selection button basic functionality

---
+++ Assets/Scripts/LevelPrototype.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New level", menuName ="Level", order =100000)]
+public class LevelPrototype : ScriptableObject {
+	public string customLabel;
+	public string number;
+	public LevelPrototype[] requirements;
+	public SceneReference scene;
+	[Multiline]
+	public string desc;
+}
---
+++ Assets/Scripts/LevelSelectionButton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelectionButton : MonoBehaviour {
+	[Header("References")]
+	public TextMeshProUGUI label;
+	public Image image;
+	public TextMeshProUGUI windowLabel;
+	public TextMeshProUGUI windowText;
+	[Header("Level")]
+	public LevelPrototype levelPrototype;
+
+
+	private void Start() {
+		label.text = levelPrototype.number;
+		windowLabel.text = levelPrototype.number + (levelPrototype.customLabel.Length > 0 ? ": " + levelPrototype.customLabel : "");
+		var desc = "";
+		if (levelPrototype.requirements.Length > 0) {
+			desc += "Requires: ";
+			foreach (var level in levelPrototype.requirements) {
+				desc += level.number;
+				if (level != levelPrototype.requirements[levelPrototype.requirements.Length - 1]) desc += ", ";
+				else desc += "\n";
+			}
+		}
+		desc += levelPrototype.desc;
+		windowText.text = desc;
+	}
+
+	public void OnClick() {
+		GameManager.instance.selectedLevel = levelPrototype.scene;
+		GameManager.instance.LoadLevel();
+	}
+
+}
