a405651fe07f746cda8f1eaa54d93372dbda2f9f
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-07-19 14:11:29 +0200

    Level loading base

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance;
+
+	public enum GameSceneState {
+		MainMenu,
+		InLevel
+	}
+	[Header("Level loading")]
+	public GameSceneState state;
+	public SceneReference selectedLevel;
+	public AsyncOperation levelLoadingOperation;
+	public GameObject playerPrefab;
+	public B.FloatEvent levelLoadingState;
+	public UnityEvent levelLoadingStarted;
+	public UnityEvent levelLoadingEnded;
+
+	private void Awake() {
+		if (!instance) {
+			instance = this;
+		} else {
+			Debug.LogError("A GameManager aleready exists, there can not be >=two");
+		}
+	}
+
+	private void Start() {
+		if (state == GameSceneState.InLevel) {
+			LoadLevel();
+		}
+	}
+
+	public void LoadLevel() {
+		if (selectedLevel.ScenePath == "") {
+			Debug.LogError("No level selected for loading");
+			return;
+		}
+		Debug.Log("Loading level: " + selectedLevel);
+		levelLoadingOperation = SceneManager.LoadSceneAsync(selectedLevel, LoadSceneMode.Additive);
+		levelLoadingStarted.Invoke();
+	}
+
+	private void Update() {
+		if (levelLoadingOperation != null) {
+			levelLoadingState.Invoke(levelLoadingOperation.progress);
+			if (levelLoadingOperation.isDone) {
+				levelLoadingEnded.Invoke();
+				levelLoadingOperation = null;
+			}
+		}
+	}
+}
