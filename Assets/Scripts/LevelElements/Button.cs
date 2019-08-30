911b893db1cfe77a09799b27cb450d62255c18a7
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-08-30 20:31:49 +0200

    Added secret room

--- Assets/Scripts/LevelElements/Button.cs
+++ Assets/Scripts/LevelElements/Button.cs
@@ -6,11 +6,18 @@ namespace LevelElements {
 	public class Button : Indicator {
 		public B.BoolEvent onStateChange;
 		public B.BoolEvent onStateChangeInverted;
+		public bool doReset;
 
 		public override void SetState(bool newState) {
 			base.SetState(newState);
 			onStateChange.Invoke(newState);
 			onStateChangeInverted.Invoke(!newState);
+
+			if (doReset) {
+				Invoke("SetFalseState", 0.01f);
+			}
 		}
+
+		public void SetFalseState() => SetState(false);
 	}
 }
