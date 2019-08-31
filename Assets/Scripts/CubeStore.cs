d5812b5947173f1140b90a675852e8bbd9e52aa4
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-08-31 21:27:55 +0200

    Fixed cube placement

--- Assets/Scripts/CubeStore.cs
+++ Assets/Scripts/CubeStore.cs
@@ -15,40 +15,56 @@ public class CubeStore : B.Controll.AbstractSink {
 			Vector3 origin = toBlock.rayOrigin.position;
 			Vector3 direction = toBlock.rayOrigin.TransformDirection(Vector3.forward);
 			Quaternion rotation = Quaternion.Euler(0, toBlock.rayOrigin.rotation.eulerAngles.y, 0);
-			if (Physics.BoxCast(origin, Vector3.one / 2f, direction, out RaycastHit hit, rotation, toBlock.maxDist, toBlock.raycastMask)) {
-				pos = hit.point + hit.normal * 0.5f;
+			bool viable = true;
+			if (Physics.Raycast(origin, direction, out RaycastHit hit, toBlock.maxDist, toBlock.raycastMask, QueryTriggerInteraction.Ignore)) {
+				pos = hit.point + hit.normal * 0.51f;
 				rotation = Quaternion.LookRotation(hit.normal, Vector3.up);
 			} else {
 				pos = origin + direction * toBlock.maxDist;
 			}
-
-			Matrix4x4 matrix = Matrix4x4.TRS(pos, rotation, Vector3.one);
-
-			Graphics.DrawMesh(previewMesh, matrix, previewMaterial, 0);
-
-			if (toInteract && !lastDown) {
-				gotCube.transform.position = pos;
-				gotCube.transform.rotation = rotation;
-				var rigidbody = gotCube.GetComponent<Rigidbody>();
-				var addCube = gotCube.GetComponent<AddCube>();
-				if (rigidbody) {
-					rigidbody.velocity = Vector3.zero;
-					rigidbody.angularVelocity = Vector3.zero;
+			System.Func<bool> checkBox = () => Physics.CheckBox(pos, Vector3.one / 2, rotation, toBlock.raycastMask, QueryTriggerInteraction.Ignore);
+			if (checkBox()) {
+				if (Physics.Raycast(origin, direction, out hit, 1000, toBlock.raycastMask, QueryTriggerInteraction.Ignore)) {
+					pos = hit.point + hit.normal * 0.51f;
+					rotation = Quaternion.LookRotation(hit.normal, Vector3.up);
+				} else {
+					pos = origin + direction * toBlock.maxDist;
 				}
-				if (addCube.isOneWorld) {
-					gotCube.layer = gameObject.layer;
-					for (var i = 0; i < gotCube.transform.childCount; i++) {
-						gotCube.transform.GetChild(i).gameObject.layer = gameObject.layer;
-					}
+				int i = 0;
+				for (; i < 5 && checkBox(); i++) {
+					pos += hit.normal * 0.1f;
 				}
-				gotCube.SetActive(true);
-				gotCube = null;
-
-
-				toBlock.enabled = true;
+				viable = !checkBox();
 			}
+			if (viable) {
+				Matrix4x4 matrix = Matrix4x4.TRS(pos, rotation, Vector3.one);
 
-			if (!toInteract) lastDown = false;
+				Graphics.DrawMesh(previewMesh, matrix, previewMaterial, 0);
+
+				if (toInteract && !lastDown) {
+					gotCube.transform.position = pos;
+					gotCube.transform.rotation = rotation;
+					var rigidbody = gotCube.GetComponent<Rigidbody>();
+					var addCube = gotCube.GetComponent<AddCube>();
+					if (rigidbody) {
+						rigidbody.velocity = Vector3.zero;
+						rigidbody.angularVelocity = Vector3.zero;
+					}
+					if (addCube.isOneWorld) {
+						gotCube.layer = gameObject.layer;
+						for (var i = 0; i < gotCube.transform.childCount; i++) {
+							gotCube.transform.GetChild(i).gameObject.layer = gameObject.layer;
+						}
+					}
+					gotCube.SetActive(true);
+					gotCube = null;
+
+
+					toBlock.enabled = true;
+				}
+
+				if (!toInteract) lastDown = false;
+			}
 		}
 
 		ResetTasks();
