5ac07c03b677dfac85434fe2c4ad86056be26985
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-08-31 20:49:54 +0200

    Added a dot to HUD center

---
+++ Assets/Scripts/HUDDot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDDot : MonoBehaviour {
+	public UnityEngine.UI.Image target;
+
+	void Update() {
+		target.enabled = LevelManager.instance != null;
+	}
+}
