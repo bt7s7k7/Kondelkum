9b6d74223fae68d943485c863ca602a0011a220b
bt7s7k7 <bt7s7k7@hotmail.sk>
2019-09-01 13:34:57 +0200

    Added a clear progress button

---
+++ Assets/Scripts/ClearProgressButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearProgressButton : MonoBehaviour {
+	public void Act() {
+		GameManager.instance.completedLevels.Clear();
+		GameManager.instance.SaveLevelCompletion();
+		GameManager.instance.LoadMainMenu();
+	}
+}
